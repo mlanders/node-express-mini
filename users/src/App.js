98ba35280114fd175008f60816eef06c7a5ed113
Mike Landers <landers.mike@gmail.com>
2019-02-11 16:02:06 -0800

    POST is working

--- users/src/App.js
+++ users/src/App.js
@@ -7,12 +7,20 @@ class App extends Component {
 		super();
 		this.state = {
 			users: [],
+			name: '',
+			bio: '',
 		};
 	}
 
 	componentDidMount() {
 		this.getUsers();
 	}
+	handleChanges = e => {
+		e.preventDefault();
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
 
 	getUsers = () => {
 		axios.get('http://localhost:5000/api/users').then(res => {
@@ -29,16 +37,49 @@ class App extends Component {
 		});
 	};
 
+	addUser = e => {
+		e.preventDefault();
+		let user = { name: this.state.name, bio: this.state.bio };
+		axios.post(`http://localhost:5000/api/users/`, user).then(res => {
+			console.log(res);
+			axios.get('http://localhost:5000/api/users').then(res => {
+				this.setState({ users: res.data.users });
+			});
+		});
+	};
+
 	render() {
 		return (
 			<div className="App">
-				{this.state.users.map(user => (
-					<div key={user.id} className="user">
-						<h1>{user.name}</h1>
-						<p>{user.bio}</p>
-						<button onClick={e => this.deleteUser(e, user.id)}>Delete</button>
-					</div>
-				))}
+				<form
+					onSubmit={e => {
+						this.addUser(e);
+					}}>
+					<input
+						name="name"
+						required
+						value={this.state.name}
+						placeholder="Name"
+						onChange={e => this.handleChanges(e)}
+					/>
+					<input
+						name="bio"
+						required
+						value={this.state.bio}
+						placeholder="Bio"
+						onChange={e => this.handleChanges(e)}
+					/>
+					<button>Submit</button>
+				</form>
+				<div className="users">
+					{this.state.users.map(user => (
+						<div key={user.id} className="user">
+							<h1>{user.name}</h1>
+							<p>{user.bio}</p>
+							<button onClick={e => this.deleteUser(e, user.id)}>Delete</button>
+						</div>
+					))}
+				</div>
 			</div>
 		);
 	}
